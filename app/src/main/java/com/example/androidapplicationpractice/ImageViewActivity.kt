c2e225b2be4eaa5342e37cf36a033852fa9b1ced
Jayas Piya <pzayazz@gmail.com>
2021-06-20 13:01:03 +0545

    Created ImageView

---
+++ app/src/main/java/com/example/androidapplicationpractice/ImageViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidapplicationpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_view)
+    }
+}
