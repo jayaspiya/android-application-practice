522b528a98bcf9cae8654b6d289411635d878966
Jayas Piya <pzayazz@gmail.com>
2021-07-04 18:27:25 +0545

    Checking Fragments

---
+++ app/src/main/java/com/example/androidapplicationpractice/FragmentActivity.kt
@@ -0,0 +1,32 @@
+package com.example.androidapplicationpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.androidapplicationpractice.fragments.AreaFragment
+import com.example.androidapplicationpractice.fragments.SumFragment
+
+class FragmentActivity : AppCompatActivity() {
+    private lateinit var btnSumFragment: Button
+    private lateinit var btnAreaFragment: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        btnSumFragment = findViewById(R.id.btnSumFragment)
+        btnAreaFragment = findViewById(R.id.btnAreaFragment)
+        btnSumFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.linearContainer, SumFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        btnAreaFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.linearContainer, AreaFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+}
